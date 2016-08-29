838442ccc0313bf645912cb3e3c599adac4ee69f
dave <dave@termx.co>
2016-08-29 16:27:12 -0600

    none

--- rule_builder/C.php
+++ rule_builder/C.php
@@ -119,439 +119,250 @@
 
 <script>
 
-var rules_widgets = {
-  condition: 'OR',
-  rules: [{
-    id: 'date',
-    operator: 'equal',
-    value: '1991/11/17'
-  }, {
-    id: 'rate',
-    operator: 'equal',
-    value: 22
-  }, {
-    id: 'category',
-    operator: 'equal',
-    value: '38'
-  }, {
-    condition: 'AND',
-    rules: [{
-      id: 'coord',
-      operator: 'equal',
-      value: 'B.3'
-    }]
-  }]
+
+$('[data-toggle="tooltip"]').tooltip();
+
+var $b = $('#builder');
+
+var options = {
+  allow_empty: true,
+
+  default_filter: 'name',
+  sort_filters: true,
+
+  optgroups: {
+    core: {
+      en: 'amp'
+    }
+  },
+
+  plugins: {
+    'bt-tooltip-errors': { delay: 100},
+    'sortable': null,
+    'filter-description': { mode: 'bootbox' },
+    'bt-selectpicker': null,
+    'unique-filter': null,
+    'bt-checkbox': { color: 'primary' },
+    'invert': null,
+    'datetimepicker':null
+  },
+
+  // standard operators in custom optgroups
+  operators: [
+    {type: 'equal',            optgroup: 'basic'},
+    {type: 'not_equal',        optgroup: 'basic'},
+    {type: 'in',               optgroup: 'basic'},
+    {type: 'not_in',           optgroup: 'basic'},
+    {type: 'less',             optgroup: 'numbers'},
+    {type: 'less_or_equal',    optgroup: 'numbers'},
+    {type: 'greater',          optgroup: 'numbers'},
+    {type: 'greater_or_equal', optgroup: 'numbers'},
+    {type: 'between',          optgroup: 'numbers'},
+    {type: 'not_between',      optgroup: 'numbers'},
+    {type: 'begins_with',      optgroup: 'strings'},
+    {type: 'not_begins_with',  optgroup: 'strings'},
+    {type: 'contains',         optgroup: 'strings'},
+    {type: 'not_contains',     optgroup: 'strings'},
+    {type: 'ends_with',        optgroup: 'strings'},
+    {type: 'not_ends_with',    optgroup: 'strings'},
+    {type: 'is_empty'     },
+    {type: 'is_not_empty' },
+    {type: 'is_null'      },
+    {type: 'is_not_null'  }
+  ],
+
+  filters: [
+
+  {
+    id: 'name',
+    label: {
+      en: 'Name'
+    },
+    type: 'string',
+    optgroup: 'amp',
+    default_value: '',
+    size: 150
+  },
+
+  {
+    id: 'price',
+    label: 'Price',
+    type: 'double',
+    size: 5,
+    validation: {
+      min: 0
+    }
+  }
+
+  ]
 };
 
-// Fix for Selectize
+// init
+$('#builder').queryBuilder(options);
+
 $('#builder').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
-  if (rule.filter.plugin == 'selectize') {
-    rule.$el.find('.rule-value-container').css('min-width', '200px')
-      .find('.selectize-control').removeClass('form-control');
+    if (rule.filter.plugin == 'selectize') {
+        rule.$el.find('.rule-value-container').css('min-width', '200px')
+          .find('.selectize-control').removeClass('form-control');
+    }
+});
+
+// change language
+$('[name=language]').selectpicker().on('change', function() {
+  var lang = $(this).val();
+
+  var done = function() {
+    var rules = $b.queryBuilder('getRules');
+    if (!$.isEmptyObject(rules)) {
+      options.rules = rules;
+    }
+    options.lang_code = lang;
+    $b.queryBuilder('destroy');
+    $('#builder').queryBuilder(options);
+  };
+
+  if ($.fn.queryBuilder.regional[lang] === undefined) {
+    $.getScript('../dist/i18n/query-builder.' + lang + '.js', done);
+  }
+  else {
+    done();
   }
 });
 
-$('#builder').queryBuilder({
-  plugins: ['bt-tooltip-errors'],
-
-  filters: [{
-    id: 'date',
-    label: 'Datepicker',
-    type: 'date',
-    validation: {
-      format: 'YYYY/MM/DD'
-    },
-    plugin: 'datepicker',
-    plugin_config: {
-      format: 'yyyy/mm/dd',
-      todayBtn: 'linked',
-      todayHighlight: true,
-      autoclose: true
-    }
-  }, {
-    id: 'rate',
-    label: 'Slider',
-    type: 'integer',
-    validation: {
-      min: 0,
-      max: 100
-    },
-    plugin: 'slider',
-    plugin_config: {
-      min: 0,
-      max: 100,
-      value: 0
-    },
-    valueSetter: function(rule, value) {
-      if (rule.operator.nb_inputs == 1) value = [value];
-      rule.$el.find('.rule-value-container input').each(function(i) {
-        $(this).slider('setValue', value[i] || 0);
-      });
-    },
-    valueGetter: function(rule) {
-      var value = [];
-      rule.$el.find('.rule-value-container input').each(function() {
-        value.push($(this).slider('getValue'));
-      });
-      return rule.operator.nb_inputs == 1 ? value[0] : value;
-    }
-  }, {
-    id: 'category',
-    label: 'Selectize',
-    type: 'string',
-    plugin: 'selectize',
-    plugin_config: {
-      valueField: 'id',
-      labelField: 'name',
-      searchField: 'name',
-      sortField: 'name',
-      create: true,
-      maxItems: 1,
-      plugins: ['remove_button'],
-      onInitialize: function() {
-        var that = this;
-
-        if (localStorage.demoData === undefined) {
-          $.getJSON(baseurl + '/assets/demo-data.json', function(data) {
-            localStorage.demoData = JSON.stringify(data);
-            data.forEach(function(item) {
-              that.addOption(item);
-            });
-          });
-        }
-        else {
-          JSON.parse(localStorage.demoData).forEach(function(item) {
-            that.addOption(item);
-          });
-        }
-      }
-    },
-    valueSetter: function(rule, value) {
-      rule.$el.find('.rule-value-container input')[0].selectize.setValue(value);
-    }
-  }, {
-    id: 'coord',
-    label: 'Coordinates',
-    type: 'string',
-    validation: {
-      format: /^[A-C]{1}.[1-6]{1}$/
-    },
-    input: function(rule, name) {
-      var $container = rule.$el.find('.rule-value-container');
-      
-      $container.on('change', '[name='+ name +'_1]', function(){
-        var h = '';
-        
-        switch ($(this).val()) {
-          case 'A':
-            h = '<option value="-1">-</option> <option value="1">1</option> <option value="2">2</option>';
-            break;
-          case 'B':
-            h = '<option value="-1">-</option> <option value="3">3</option> <option value="4">4</option>';
-            break;
-          case 'C':
-            h = '<option value="-1">-</option> <option value="5">5</option> <option value="6">6</option>';
-            break;
-        }
-        
-        $container.find('[name='+ name +'_2]').html(h).toggle(h!='');
-      });
-      
-      return '\
-      <select name="'+ name +'_1"> \
-        <option value="-1">-</option> \
-        <option value="A">A</option> \
-        <option value="B">B</option> \
-        <option value="C">C</option> \
-      </select> \
-      <select name="'+ name +'_2" style="display:none;"></select>';
-    },
-    valueGetter: function(rule) {
-      return rule.$el.find('.rule-value-container [name$=_1]').val()
-        +'.'+ rule.$el.find('.rule-value-container [name$=_2]').val();
-    },
-    valueSetter: function(rule, value) {
-      if (rule.operator.nb_inputs > 0) {
-        var val = value.split('.');
-        
-        rule.$el.find('.rule-value-container [name$=_1]').val(val[0]).trigger('change');
-        rule.$el.find('.rule-value-container [name$=_2]').val(val[1]).trigger('change');
-      }
-    }
-  }],
-
-  rules: rules_widgets
+// change theme
+$('.change-theme').on('click', function() {
+    $('#qb-theme').replaceWith('<link rel="stylesheet" href="' + $(this).data('qb') + '" id="qb-theme">');
+    $('#bt-theme').replaceWith('<link rel="stylesheet" href="' + $(this).data('bt') + '" id="bt-theme">');
 });
 
-$('#btn-reset').on('click', function() {
+// set rules
+$('.set').on('click', function() {
+  $('#builder').queryBuilder('setRules', {
+    condition: 'AND',
+    flags: {
+      condition_readonly: true
+    },
+    rules: [{
+      id: 'price',
+      operator: 'between',
+      value: [10.25, 15.52],
+      flags: {
+        no_delete: true,
+        filter_readonly: true
+      },
+      data: {
+        unit: '€'
+      }
+    }, {
+      id: 'state',
+      operator: 'equal',
+      value: 'AK'
+    }, {
+      condition: 'OR',
+      flags: {
+        no_delete: true
+      },
+      rules: [{
+        id: 'category',
+        operator: 'equal',
+        value: 2
+      }, {
+        id: 'coord',
+        operator: 'equal',
+        value: 'B.3'
+      }]
+    }]
+  });
+});
+
+// set rules from MongoDB
+$('.set-mongo').on('click', function() {
+  $('#builder').queryBuilder('setRulesFromMongo', {
+    "$and": [{
+      "name": {
+        "$regex": "^(?!Mistic)"
+      }
+    }, {
+      "price": { "$gte": 0, "$lte": 100 }
+    }, {
+      "$or": [{
+        "category": 2
+      }, {
+        "category": { "$in": [4, 5] }
+      }]
+    }]
+  });
+});
+
+// set rules from SQL
+$('.set-sql').on('click', function() {
+  $('#builder').queryBuilder('setRulesFromSQL', 'name NOT LIKE "Mistic%" AND price BETWEEN 100 AND 200 AND (category IN(1, 2) OR rate <= 2)');
+});
+
+// reset builder
+$('.reset').on('click', function() {
   $('#builder').queryBuilder('reset');
+  $('#result').addClass('hide').find('pre').empty();
 });
 
-$('#btn-set').on('click', function() {
-  $('#builder').queryBuilder('setRules', rules_widgets);
+// get rules
+$('.parse-json').on('click', function() {
+  $('#result').removeClass('hide')
+    .find('pre').html(JSON.stringify(
+      $('#builder').queryBuilder('getRules', {get_flags: true}),
+      undefined, 2
+    ));
 });
 
-$('#btn-get').on('click', function() {
-  var result = $('#builder').queryBuilder('getRules');
-  
-  if (!$.isEmptyObject(result)) {
-    alert(JSON.stringify(result, null, 2));
-  }
+$('.parse-sql').on('click', function() {
+  var res = $('#builder').queryBuilder('getSQL', $(this).data('stmt'), false);
+  $('#result').removeClass('hide')
+    .find('pre').html(
+      res.sql + (res.params ? '\n\n' + JSON.stringify(res.params, undefined, 2) : '')
+    );
+});
+
+$('.parse-mongo').on('click', function() {
+  $('#result').removeClass('hide')
+    .find('pre').html(JSON.stringify(
+      $('#builder').queryBuilder('getMongo'),
+      undefined, 2
+    ));
+});
+
+// set filters
+$('.set-filters').on('click', function() {
+  $(this).prop('disabled', true);
+  $(this).tooltip('hide');
+
+  // add a new filter after 'state'
+  $('#builder').queryBuilder('addFilter',
+    {
+      id: 'new_one',
+      label: 'New filter',
+      type: 'string'
+    },
+    'state'
+  );
+
+  // remove filter 'coord'
+  $('#builder').queryBuilder('removeFilter',
+    ['coord', 'state', 'bson'],
+    true
+  );
+
+  // also available : 'setFilters'
 });
 
 
-
-
-// $('[data-toggle="tooltip"]').tooltip();
-
-// var $b = $('#builder');
-
-// var options = {
-//   allow_empty: true,
-
-//   default_filter: 'name',
-//   sort_filters: true,
-
-//   optgroups: {
-//     core: {
-//       en: 'amp'
-//     }
-//   },
-
-//   plugins: {
-//     'bt-tooltip-errors': { delay: 100},
-//     'sortable': null,
-//     'filter-description': { mode: 'bootbox' },
-//     'bt-selectpicker': null,
-//     'unique-filter': null,
-//     'bt-checkbox': { color: 'primary' },
-//     'invert': null,
-//     'datetimepicker':null
-//   },
-
-//   // standard operators in custom optgroups
-//   operators: [
-//     {type: 'equal',            optgroup: 'basic'},
-//     {type: 'not_equal',        optgroup: 'basic'},
-//     {type: 'in',               optgroup: 'basic'},
-//     {type: 'not_in',           optgroup: 'basic'},
-//     {type: 'less',             optgroup: 'numbers'},
-//     {type: 'less_or_equal',    optgroup: 'numbers'},
-//     {type: 'greater',          optgroup: 'numbers'},
-//     {type: 'greater_or_equal', optgroup: 'numbers'},
-//     {type: 'between',          optgroup: 'numbers'},
-//     {type: 'not_between',      optgroup: 'numbers'},
-//     {type: 'begins_with',      optgroup: 'strings'},
-//     {type: 'not_begins_with',  optgroup: 'strings'},
-//     {type: 'contains',         optgroup: 'strings'},
-//     {type: 'not_contains',     optgroup: 'strings'},
-//     {type: 'ends_with',        optgroup: 'strings'},
-//     {type: 'not_ends_with',    optgroup: 'strings'},
-//     {type: 'is_empty'     },
-//     {type: 'is_not_empty' },
-//     {type: 'is_null'      },
-//     {type: 'is_not_null'  }
-//   ],
-
-//   filters: [
-
-//   {
-//     id: 'name',
-//     label: {
-//       en: 'Name'
-//     },
-//     type: 'string',
-//     optgroup: 'amp',
-//     default_value: '',
-//     size: 150
-//   },
-
-//   {
-//     id: 'date',
-//     label: 'Datepicker',
-//     type: 'date',
-//     validation: {
-//       format: 'YYYY/MM/DD'
-//     },
-//     optgroup: 'amp',
-//     plugin: 'datepicker',
-//     plugin_config: {
-//       format: 'yyyy/mm/dd',
-//       todayBtn: 'linked',
-//       todayHighlight: true,
-//       autoclose: true
-//     }
-//   }
-
-//   ]
-// };
-
-// // init
-// $('#builder').queryBuilder(options);
-
-// $('#builder').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
-//     if (rule.filter.plugin == 'selectize') {
-//         rule.$el.find('.rule-value-container').css('min-width', '200px')
-//           .find('.selectize-control').removeClass('form-control');
-//     }
-// });
-
-// // change language
-// $('[name=language]').selectpicker().on('change', function() {
-//   var lang = $(this).val();
-
-//   var done = function() {
-//     var rules = $b.queryBuilder('getRules');
-//     if (!$.isEmptyObject(rules)) {
-//       options.rules = rules;
-//     }
-//     options.lang_code = lang;
-//     $b.queryBuilder('destroy');
-//     $('#builder').queryBuilder(options);
-//   };
-
-//   if ($.fn.queryBuilder.regional[lang] === undefined) {
-//     $.getScript('../dist/i18n/query-builder.' + lang + '.js', done);
-//   }
-//   else {
-//     done();
-//   }
-// });
-
-// // change theme
-// $('.change-theme').on('click', function() {
-//     $('#qb-theme').replaceWith('<link rel="stylesheet" href="' + $(this).data('qb') + '" id="qb-theme">');
-//     $('#bt-theme').replaceWith('<link rel="stylesheet" href="' + $(this).data('bt') + '" id="bt-theme">');
-// });
-
-// // set rules
-// $('.set').on('click', function() {
-//   $('#builder').queryBuilder('setRules', {
-//     condition: 'AND',
-//     flags: {
-//       condition_readonly: true
-//     },
-//     rules: [{
-//       id: 'price',
-//       operator: 'between',
-//       value: [10.25, 15.52],
-//       flags: {
-//         no_delete: true,
-//         filter_readonly: true
-//       },
-//       data: {
-//         unit: '€'
-//       }
-//     }, {
-//       id: 'state',
-//       operator: 'equal',
-//       value: 'AK'
-//     }, {
-//       condition: 'OR',
-//       flags: {
-//         no_delete: true
-//       },
-//       rules: [{
-//         id: 'category',
-//         operator: 'equal',
-//         value: 2
-//       }, {
-//         id: 'coord',
-//         operator: 'equal',
-//         value: 'B.3'
-//       }]
-//     }]
-//   });
-// });
-
-// // set rules from MongoDB
-// $('.set-mongo').on('click', function() {
-//   $('#builder').queryBuilder('setRulesFromMongo', {
-//     "$and": [{
-//       "name": {
-//         "$regex": "^(?!Mistic)"
-//       }
-//     }, {
-//       "price": { "$gte": 0, "$lte": 100 }
-//     }, {
-//       "$or": [{
-//         "category": 2
-//       }, {
-//         "category": { "$in": [4, 5] }
-//       }]
-//     }]
-//   });
-// });
-
-// // set rules from SQL
-// $('.set-sql').on('click', function() {
-//   $('#builder').queryBuilder('setRulesFromSQL', 'name NOT LIKE "Mistic%" AND price BETWEEN 100 AND 200 AND (category IN(1, 2) OR rate <= 2)');
-// });
-
-// // reset builder
-// $('.reset').on('click', function() {
-//   $('#builder').queryBuilder('reset');
-//   $('#result').addClass('hide').find('pre').empty();
-// });
-
-// // get rules
-// $('.parse-json').on('click', function() {
-//   $('#result').removeClass('hide')
-//     .find('pre').html(JSON.stringify(
-//       $('#builder').queryBuilder('getRules', {get_flags: true}),
-//       undefined, 2
-//     ));
-// });
-
-// $('.parse-sql').on('click', function() {
-//   var res = $('#builder').queryBuilder('getSQL', $(this).data('stmt'), false);
-//   $('#result').removeClass('hide')
-//     .find('pre').html(
-//       res.sql + (res.params ? '\n\n' + JSON.stringify(res.params, undefined, 2) : '')
-//     );
-// });
-
-// $('.parse-mongo').on('click', function() {
-//   $('#result').removeClass('hide')
-//     .find('pre').html(JSON.stringify(
-//       $('#builder').queryBuilder('getMongo'),
-//       undefined, 2
-//     ));
-// });
-
-// // set filters
-// $('.set-filters').on('click', function() {
-//   $(this).prop('disabled', true);
-//   $(this).tooltip('hide');
-
-//   // add a new filter after 'state'
-//   $('#builder').queryBuilder('addFilter',
-//     {
-//       id: 'new_one',
-//       label: 'New filter',
-//       type: 'string'
-//     },
-//     'state'
-//   );
-
-//   // remove filter 'coord'
-//   $('#builder').queryBuilder('removeFilter',
-//     ['coord', 'state', 'bson'],
-//     true
-//   );
-
-//   // also available : 'setFilters'
-// });
-
-
-// $('#builder').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
-//     if (rule.filter.id === 'date') {
-//       var $input = rule.$el.find('.rule-value-container [name*=_value_]');
-//       $input.on('dp.change', function() {
-//           $input.trigger('change');
-//       });
-//     }
-// });
+$('#builder').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
+    if (rule.filter.id === 'date') {
+      var $input = rule.$el.find('.rule-value-container [name*=_value_]');
+      $input.on('dp.change', function() {
+          $input.trigger('change');
+      });
+    }
+});
 
 </script>
 

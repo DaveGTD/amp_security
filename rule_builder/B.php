0cc61be86b7b257dda522e861e1d4fca45fdb094
dave <dave@termx.co>
2016-08-29 18:06:03 -0600

    none

--- rule_builder/B.php
+++ rule_builder/B.php
@@ -1,8 +1,33 @@
 <?php 
-echo $_POST["hidden_query"]; 
-echo "<br>";
-echo $_POST["tag"];
+// echo $_POST["hidden_query"]; 
+// echo "<br>";
+// echo $_POST["tag"];
 
+$query_given = $_POST["hidden_query"];
+$tag_given = $_POST["tag"];
 
+$servername = "localhost";
+$username = "amp";
+$password = "amp";
+$dbname = "amp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO amp (tag, query) VALUES ("$tag", "$query_given")";
+
+if ($conn->query($sql) === TRUE) 
+{
+   echo "New record created successfully";
+} else {
+   echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 
 ?>
